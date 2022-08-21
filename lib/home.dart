695351d38263508ca45d187e6f6a8e80fff6a457
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2022-08-21 21:10:16 +0200

    search bar and cart completed

--- lib/home.dart
+++ lib/home.dart
@@ -1,32 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:la_vie_app_1/cart.dart' as cart_page;
+import 'package:la_vie_app_1/search.dart' as search_page;
 
-// الصفحه اللي هيكون فيها النباتات ومانساش اني اغير اسم ال كلاس
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  final Color myGreen = const Color.fromRGBO(26, 188, 0, 1);
+  final Color textGrey = const Color.fromRGBO(147, 147, 147, 1);
+  final Color dividerGrey = const Color.fromRGBO(151, 151, 151, 1);
+  final Color searchBackground = const Color.fromRGBO(221, 222, 225, 0.4);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          alignment: Alignment.center,
-          color: Colors.white,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.fromLTRB(20, 50, 20, 50),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Text("this is text 1"),
-                    Text("this is text 2"),
-                  ]),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Text("this is text 3"),
-                    Text("this is text 4"),
-                  ]),
+              //Logo
+              Align(
+                alignment: Alignment.topCenter,
+                child: SvgPicture.asset(
+                  "images/Logo.svg",
+                  width: 120.0,
+                  height: 43.0,
+                ),
+              ),
+
+              //search bar and cart button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 25, 0, 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    //Search Bar
+                    SizedBox(
+                      width: 250,
+                      height: 40.0,
+                      //height: ,
+                      child: TextFormField(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const search_page.Search(),
+                            ),
+                          );
+                        },
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: searchBackground,
+                          contentPadding: const EdgeInsets.all(0),
+                          hintText: "Search",
+                          hintStyle: TextStyle(
+                            color: dividerGrey,
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search_outlined,
+                            color: dividerGrey,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: searchBackground,
+                            ),
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: searchBackground,
+                            ),
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    //Cart Button
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: myGreen,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const cart_page.Cart(),
+                          ),
+                        );
+                      },
+                      child: const Icon(
+                        Icons.shopping_cart_outlined,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // Space
+              const SizedBox(
+                height: 10.0,
+              ),
             ],
           ),
         ),
