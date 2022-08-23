10881a8f8e0e1890fb2eb53ae9a7e4f50d819187
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2022-08-23 19:53:47 +0200

    App Icon and name changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:la_vie_app_1/main_splash_screen.dart' as splash_screen;
+import 'package:la_vie_app_1/home.dart' as home_page;
+import 'package:la_vie_app_1/test.dart' as test;
 
 void main() {
   runApp(const MyApp());
@@ -30,11 +32,27 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: "La Vie",
+      color: Colors.white,
       theme: ThemeData(
         primarySwatch: Palette.greenToDark,
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: ButtonStyle(
+            side: MaterialStateProperty.all(
+              const BorderSide(
+                color: Color(0xFF1ABC00),
+              ),
+            ),
+          ),
+        ),
       ),
       home: const Scaffold(
-        body: splash_screen.MainSplashScreen(),
+        body: home_page.HomePage(),
+        //splash_screen.MainSplashScreen(),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+        ),
       ),
     );
   }
