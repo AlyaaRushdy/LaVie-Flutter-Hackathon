5533a85ddd1dc8fbea42c9ddd508427aed4a45c5
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2022-08-24 00:59:22 +0200

    created a class to contain all the colors and removed them from indvidual pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:la_vie_app_1/project_colors.dart';
 import 'package:la_vie_app_1/main_splash_screen.dart' as splash_screen;
 import 'package:la_vie_app_1/home.dart' as home_page;
 import 'package:la_vie_app_1/test.dart' as test;
 
 void main() {
   runApp(const MyApp());
-}
-
-class Palette {
-  static const MaterialColor greenToDark = MaterialColor(
-    0xFF1ABC00, // 0% comes in here, this will be color picked if no shade is selected when defining a Color property which doesn’t require a swatch.
-    <int, Color>{
-      50: Color(0xff17a900), //10%
-      100: Color(0xff159600), //20%
-      200: Color(0xff128400), //30%
-      300: Color(0xff107100), //40%
-      400: Color(0xff0d5e00), //50%
-      500: Color(0xff0a4b00), //60%
-      600: Color(0xff083800), //70%
-      700: Color(0xff052600), //80%
-      800: Color(0xff031300), //90%
-      900: Color(0xff000000), //100%
-    },
-  );
 }
 
 class MyApp extends StatelessWidget {
@@ -46,13 +29,13 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const Scaffold(
+      home: Scaffold(
         body: home_page.HomePage(),
         //splash_screen.MainSplashScreen(),
 
-        floatingActionButton: FloatingActionButton(
+        /* floatingActionButton: FloatingActionButton(
           onPressed: () {},
-        ),
+        ), */
       ),
     );
   }
