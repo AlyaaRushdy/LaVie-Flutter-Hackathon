bfb8b4d613fe00713b36ed8afb4647d561b91f62
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2022-08-24 00:56:30 +0200

    Cart page full

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:la_vie_app_1/project_colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:la_vie_app_1/home.dart' as home_page;
 
 class Cart extends StatelessWidget {
   const Cart({Key? key}) : super(key: key);
 
-  final Color myGreen = const Color.fromRGBO(26, 188, 0, 1);
-  final Color textGrey = const Color.fromRGBO(147, 147, 147, 1);
-  final Color dividerGrey = const Color.fromRGBO(151, 151, 151, 1);
-  final Color searchBackground = const Color.fromRGBO(221, 222, 225, 0.4);
+  final SizedBox w10Space = const SizedBox(
+    width: 10,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: Container(
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 30),
+        width: double.infinity,
+        child: FloatingActionButton(
+          backgroundColor: myGreen,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: const Text(
+            "Checkout",
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () {},
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: Container(
         margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
         child: SingleChildScrollView(
@@ -21,19 +38,29 @@ class Cart extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  SizedBox(
-                    width: 30,
+                  //Back Icon
+                  Expanded(
                     child: IconButton(
-                      onPressed: () {},
+                      alignment: Alignment.centerLeft,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => home_page.HomePage(),
+                          ),
+                        );
+                      },
                       icon: const Icon(Icons.arrow_back_rounded),
                     ),
                   ),
-                  SizedBox(
-                    width: 250,
-                    child: TextFormField(
+
+                  //Cart header text
+                  const Expanded(
+                    flex: 8,
+                    child: TextField(
                       enabled: false,
                       textAlign: TextAlign.center,
-                      decoration: const InputDecoration(
+                      decoration: InputDecoration(
                         border: InputBorder.none,
                         hintText: "My Cart",
                         hintStyle: TextStyle(
@@ -48,35 +75,38 @@ class Cart extends StatelessWidget {
               ),
 
               //space
-              const SizedBox(
-                height: 10,
-              ),
+              w10Space,
 
-              Card(
-                shadowColor: Colors.grey,
-                elevation: 2,
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: 130,
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: SvgPicture.asset(
-                          "images/LogInDecoration.svg",
-                          width: 120,
-                          height: 120,
+              //Item Detailes
+              IntrinsicHeight(
+                child: Card(
+                  shadowColor: Colors.grey,
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Row(
+                    children: [
+                      //item image
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SvgPicture.asset(
+                            alignment: Alignment.center,
+                            "images/Logo.svg",
+                            width: 140,
+                            height: 140,
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 150,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 20, horizontal: 0),
+
+                      //item info
+                      Expanded(
                         child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
+                            //name
                             const Text(
                               "Cactus Plant",
                               style: TextStyle(
@@ -84,45 +114,262 @@ class Cart extends StatelessWidget {
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                            Text(
+
+                            //price
+                            const Text(
                               "200 EGP",
                               style: TextStyle(
                                 fontSize: 16,
                                 color: myGreen,
                               ),
                             ),
-                            Align(
-                              alignment: Alignment.bottomLeft,
-                              child: SizedBox(
-                                width: 100,
-                                height: 35,
-                                child: ElevatedButton(
-                                  child: const Text(
-                                    "1",
-                                    style: TextStyle(color: Colors.white),
+
+                            //amount, increment and decrement buttons and delete button
+                            Row(
+                              children: [
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  decoration: BoxDecoration(
+                                    color: searchBackground,
+                                    borderRadius: BorderRadius.circular(20),
                                   ),
-                                  onPressed: () {},
+
+                                  //amount , increment and decrement buttons
+                                  child: Row(
+                                    children: [
+                                      IconButton(
+                                        color: myGreen,
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.add),
+                                      ),
+                                      const Text(
+                                        "1",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      IconButton(
+                                        color: myGreen,
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.remove),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
+
+                                //delete button
+                                Expanded(
+                                  child: IconButton(
+                                    alignment: Alignment.centerRight,
+                                    color: myGreen,
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.delete),
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
 
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    child: const Text(
-                      "Checkout",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () {},
+              //Item Detailes
+              IntrinsicHeight(
+                child: Card(
+                  shadowColor: Colors.grey,
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Row(
+                    children: [
+                      //item image
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SvgPicture.asset(
+                            alignment: Alignment.center,
+                            "images/Logo.svg",
+                            width: 140,
+                            height: 140,
+                          ),
+                        ),
+                      ),
+
+                      //item info
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            //name
+                            const Text(
+                              "Cactus Plant",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+
+                            //price
+                            const Text(
+                              "200 EGP",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: myGreen,
+                              ),
+                            ),
+
+                            //amount, increment and decrement buttons and delete button
+                            Row(
+                              children: [
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  decoration: BoxDecoration(
+                                    color: searchBackground,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+
+                                  //amount , increment and decrement buttons
+                                  child: Row(
+                                    children: [
+                                      IconButton(
+                                        color: myGreen,
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.add),
+                                      ),
+                                      const Text(
+                                        "1",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      IconButton(
+                                        color: myGreen,
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.remove),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+
+                                //delete button
+                                Expanded(
+                                  child: IconButton(
+                                    alignment: Alignment.centerRight,
+                                    color: myGreen,
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.delete),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              //Item Detailes
+              IntrinsicHeight(
+                child: Card(
+                  shadowColor: Colors.grey,
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Row(
+                    children: [
+                      //item image
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SvgPicture.asset(
+                            alignment: Alignment.center,
+                            "images/Logo.svg",
+                            width: 140,
+                            height: 140,
+                          ),
+                        ),
+                      ),
+
+                      //item info
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            //name
+                            const Text(
+                              "Cactus Plant",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+
+                            //price
+                            const Text(
+                              "200 EGP",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: myGreen,
+                              ),
+                            ),
+
+                            //amount, increment and decrement buttons and delete button
+                            Row(
+                              children: [
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  decoration: BoxDecoration(
+                                    color: searchBackground,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+
+                                  //amount , increment and decrement buttons
+                                  child: Row(
+                                    children: [
+                                      IconButton(
+                                        color: myGreen,
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.add),
+                                      ),
+                                      const Text(
+                                        "1",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      IconButton(
+                                        color: myGreen,
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.remove),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+
+                                //delete button
+                                Expanded(
+                                  child: IconButton(
+                                    alignment: Alignment.centerRight,
+                                    color: myGreen,
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.delete),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
